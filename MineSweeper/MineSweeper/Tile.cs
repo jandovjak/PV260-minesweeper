c87442917078f584aa945d5b7aa746e67ecc225a
Ján Dovjak <xdovjak@fi.muni.cz>
2021-03-29 16:51:48 +0200

    Tile class

---
+++ MineSweeper/MineSweeper/Tile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    public class Tile
+    {
+        public bool IsBomb { get; set; }
+        public int BombsAround { get; set; }
+        public bool IsReviewed { get; set; }
+    }
+}
