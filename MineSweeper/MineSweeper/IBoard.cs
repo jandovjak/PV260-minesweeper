b99e39b27a7f114d078e19db7e2f054aa7a85aa4
Eduard Šrol <eduardsrol@gmail.com>
2021-04-08 18:42:59 +0200

    Initialize added to interface

--- MineSweeper/MineSweeper/IBoard.cs
+++ MineSweeper/MineSweeper/IBoard.cs
@@ -6,6 +6,7 @@ namespace MineSweeper
         public int Width { get; }
         public ITile GetTile(int x, int y);
         public bool IsValidPosition(int x, int y);
+        public void Initialize();
         public string ToString();
 
     }
