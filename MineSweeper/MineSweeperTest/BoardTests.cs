abdf6eb8e71c5fb8a5cabd33c2f39d92415fc9e3
Ján Dovjak <xdovjak@fi.muni.cz>
2021-04-08 20:11:24 +0200

    Another tests for board created

--- MineSweeper/MineSweeperTest/BoardTests.cs
+++ MineSweeper/MineSweeperTest/BoardTests.cs
@@ -148,6 +148,99 @@ namespace MineSweeperTest
             Assert.AreEqual(21, numRevealedTilesAfter);
         }
         
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(0, 2)]
+        [TestCase(1, 0)]
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 0)]
+        [TestCase(2, 1)]
+        [TestCase(2, 2)]
+        public void CheckRevealTile_GivenTileIsEmpty_EmptyBoard5x5(int x, int y)
+        {
+            var board = emptyBoard3x3();
+            board.RevealTile(x, y);
+            var numRevealedTilesAfter = board.Tiles.Count(tile => tile.IsRevealed);
+            Assert.AreEqual(9, numRevealedTilesAfter);
+        }
+        
+        [Test]
+        [TestCase(2, 0)]
+        [TestCase(2, 1)]
+        [TestCase(2, 2)]
+        [TestCase(2, 3)]
+        [TestCase(2, 4)]
+        public void CheckRevealTile_GivenTileIsBomb_Board5x5BombsInMiddleRow(int x, int y)
+        {
+            var board = board5x5BombsInMiddleRow();
+            var tiles = board.Tiles;
+            
+            board.RevealTile(x, y);
+            Assert.IsTrue(board.GetTile(x, y).IsRevealed);
+
+            int numRevealedTilesAfter = tiles.Count(tile => tile.IsRevealed);
+            Assert.AreEqual(1, numRevealedTilesAfter);
+        }
+        
+        [Test]
+        [TestCase(1, 0)]
+        [TestCase(3, 0)]
+        [TestCase(1, 1)]
+        [TestCase(3, 1)]
+        [TestCase(1, 2)]
+        [TestCase(3, 2)]
+        [TestCase(1, 3)]
+        [TestCase(3, 3)]
+        [TestCase(1, 4)]
+        [TestCase(3, 4)]
+        public void CheckRevealTile_GivenTileNextToBomb_Board5x5BombsInMiddleRow(int x, int y)
+        {
+            var board = board5x5BombsInMiddleRow();
+            var tiles = board.Tiles;
+            board.RevealTile(x, y);
+            Assert.IsTrue(board.GetTile(x, y).IsRevealed);
+            int numRevealedTilesAfter = tiles.Count(tile => tile.IsRevealed);
+            Assert.AreEqual(1, numRevealedTilesAfter);
+        }
+        
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(0, 2)]
+        [TestCase(0, 3)]
+        [TestCase(0, 4)]
+        public void CheckRevealTile_GivenTileIsEmptyTileInUpperPart_Board5x5BombsInMiddleRow(int x, int y)
+        {
+            var board = board5x5BombsInMiddleRow();
+            var tiles = board.Tiles;
+            board.RevealTile(x, y);
+            Assert.IsTrue(board.GetTile(x, y).IsRevealed);
+            var revealedTilesOnBoard = tiles.Count(tile => tile.IsRevealed);
+            Assert.AreEqual(10, revealedTilesOnBoard);
+            var revealedTilesOnBoardInUpperPart = tiles.Take(15).Count(tile => tile.IsRevealed);
+            Assert.AreEqual(10, revealedTilesOnBoardInUpperPart);
+        }
+        
+        [Test]
+        [TestCase(4, 0)]
+        [TestCase(4, 1)]
+        [TestCase(4, 2)]
+        [TestCase(4, 3)]
+        [TestCase(4, 4)]
+        public void CheckRevealTile_GivenTileIsEmptyTileInLowerPart_Board5x5BombsInMiddleRow(int x, int y)
+        {
+            var board = board5x5BombsInMiddleRow();
+            var tiles = board.Tiles;
+            board.RevealTile(x, y);
+            Assert.IsTrue(board.GetTile(x, y).IsRevealed);
+            var revealedTilesOnBoard = tiles.Count(tile => tile.IsRevealed);
+            Assert.AreEqual(10, revealedTilesOnBoard);
+            var revealedTilesOnBoardInUpperPart = tiles.Skip(10).Count(tile => tile.IsRevealed);
+            Assert.AreEqual(10, revealedTilesOnBoardInUpperPart);
+        }
+        
         private Board board5x5BombsInAllCorners()
         {
             var board = new Board(5, 5);
@@ -167,6 +260,33 @@ namespace MineSweeperTest
             board.GetTile(3, 0).BombsAround = 1;
             board.GetTile(3, 1).BombsAround = 1;
             board.GetTile(4, 1).BombsAround = 1;
+            return board;
+        }
+        
+        private Board board5x5BombsInMiddleRow()
+        {
+            var board = new Board(5, 5);
+            board.GetTile(2, 0).IsBomb = true;
+            board.GetTile(1, 0).BombsAround = 2;
+            board.GetTile(3, 0).BombsAround = 2;
+            board.GetTile(2, 1).IsBomb = true;
+            board.GetTile(1, 1).BombsAround = 3;
+            board.GetTile(3, 1).BombsAround = 3;
+            board.GetTile(2, 2).IsBomb = true;
+            board.GetTile(1, 2).BombsAround = 3;
+            board.GetTile(3, 2).BombsAround = 3;
+            board.GetTile(2, 3).IsBomb = true;
+            board.GetTile(1, 3).BombsAround = 3;
+            board.GetTile(3, 3).BombsAround = 3;
+            board.GetTile(2,4).IsBomb = true;
+            board.GetTile(1, 4).BombsAround = 2;
+            board.GetTile(3, 4).BombsAround = 2;
+            return board;
+        }
+        
+        private Board emptyBoard3x3()
+        {
+            var board = new Board(3, 3);
             return board;
         }
     }
